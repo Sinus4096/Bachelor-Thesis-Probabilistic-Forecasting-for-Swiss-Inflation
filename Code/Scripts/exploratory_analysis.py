995768dd1ae50bee3bc06a2458945650e816b9a4
Sinus4096 <sinamaria.schlegel@student.unisg.ch>
2025-10-27 18:19:13 +0100

    commit changes

--- Code/Scripts/exploratory_analysis.py
+++ Code/Scripts/exploratory_analysis.py
@@ -5,6 +5,23 @@ import seaborn as sns
 
 #do 3,6,9 and 12 month forecasts-> long and short term
 #compare with out of sample method-> data got up to then model's recursive forecasting vintages, find the corresponding benchmark vintage
+#need to extract point forecast if want to compare with forecast of snb???
+#add dummy for covid? some motnhs before will know when have things closed
+#ai studios says train data til 2006 und gemini til 2009-> between
+#default facories fürs modeln
+#hyperparameter with optuna
+
+'''Recursive Updating Scheme:
+Frequency: The paper updates quarterly (every three months). You should follow this, as inflation dynamics often have a quarterly rhythm, and it makes computational sense.
+Mechanism:
+Vintage 1: Train your model using data from January 2001 up to your chosen initial cutoff (e.g., December 2005). Generate forecasts for horizons t+3, t+6, t+9, t+12 months (i.e., for Q1, Q2, Q3, Q4 2006).
+Vintage 2: Add one quarter of data (January-March 2006). Retrain the model using data from January 2001 up to March 2006. Generate forecasts for Q2, Q3, Q4 2006 and Q1 2007.
+Continue: Repeat this process, adding three months of data to your training set and re-estimating the model, until your evaluation sample (up to December 2025) is exhausted.
+Out-of-Sample Evaluation Period:
+This will start after your initial training period. If you train until December 2005, your first out-of-sample forecasts will be for early 2006.
+Your evaluation sample will range from January 2006 (or wherever your first forecast begins) until December 2025.'''
+
+
 
 path = 'Code/Data/Cleaned_Data/QRF_data.csv'
 df = pd.read_csv(path, index_col='Date', parse_dates=True)
